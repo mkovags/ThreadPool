c0baafb1df1d9080b23eb4b9df24284f47df5a08
Márcio Kovags <you@example.com>
2022-03-05 09:37:34 +0000

    Clang-tidied Add documentation Fix logic error

--- ThreadPool.h
+++ ThreadPool.h
@@ -12,8 +12,7 @@ class FunctionWrapper
 	struct ImplBase
 	{
 		virtual void call() = 0;
-		virtual ~ImplBase()
-		{}
+		virtual ~ImplBase() = default;
 	};
 	std::unique_ptr<ImplBase> mImpl;
 
@@ -21,27 +20,27 @@ class FunctionWrapper
 	struct ImplType: ImplBase
 	{
 		F mFunc;
-		ImplType(F &&func)
+		explicit ImplType(F &&func)
 			: mFunc{std::move(func)}
 		{}
-		void call()
+		void call() override
 		{ mFunc(); }
 	};
 
 public:
 	template<typename F>
 	FunctionWrapper(F &&func)
-		: mImpl{new ImplType<F>(std::move(func))}
+		: mImpl{new ImplType<F>(std::forward<F>(func))}
 	{}
 
 	void operator()()
 	{ mImpl->call(); }
 	FunctionWrapper() = default;
-	FunctionWrapper(FunctionWrapper &&rhs)
+	FunctionWrapper(FunctionWrapper &&rhs) noexcept
 		: mImpl{std::move(rhs.mImpl)}
 	{}
 
-	FunctionWrapper &operator=(FunctionWrapper &&rhs)
+	FunctionWrapper &operator=(FunctionWrapper &&rhs) noexcept
 	{
 		mImpl = std::move(rhs.mImpl);
 		return *this;
@@ -50,22 +49,36 @@ public:
 	FunctionWrapper(const FunctionWrapper &) = delete;
 	FunctionWrapper(FunctionWrapper &) = delete;
 	FunctionWrapper &operator=(const FunctionWrapper &) = delete;
-	~FunctionWrapper()
-	{}
+	~FunctionWrapper() = default;
 };
 
+/**
+ * This thread pool starts by working in "fast mode", if a thread spends too much time without getting work it will
+ * sleep on a condition variable and reawaken to "fast mode" again when it gets more work
+ */
 class ThreadPool
 {
 public:
-	ThreadPool(unsigned numThreads = std::thread::hardware_concurrency(), bool waitForAllTasksToFinish = false)
+	/**
+	 *
+	 * @param numThreads Number of threads in the thread pool. By default it takes a hint of from the implementation,
+	 * if the value isn't well defined or computable, it defaults to 2.
+	 * @param waitForAllTasksToFinish The constructor will wait until all the work on the queue has been processed before
+	 * it shuts down
+	 */
+	explicit ThreadPool(unsigned numThreads = std::thread::hardware_concurrency(), bool waitForAllTasksToFinish = false)
 		:
 		mDone{false},
 		mWaitForAllTasksToFinish{waitForAllTasksToFinish}
 	{
+		if (numThreads == 0) {
+			numThreads = 2;
+		}
+
 		mThreads.reserve(numThreads);
 		try {
 			for (unsigned i = 0; i != numThreads; ++i) {
-				mThreads.push_back(std::thread(&ThreadPool::workerThread, this));
+				mThreads.emplace_back(std::thread(&ThreadPool::workerThread, this));
 			}
 		}
 		catch (...) {
@@ -76,11 +89,16 @@ public:
 		}
 	}
 
+	/**
+	 * @brief Submits fn to the queue
+	 * @param fn Function to be executed on the thread queue
+	 * @return A future that can be used to check the value once it's been processed by the thread queue
+	 */
 	template<typename FunctionType>
-	std::future<typename std::result_of<FunctionType()>::type> submit(FunctionType f)
+	std::future<typename std::result_of<FunctionType()>::type> submit(FunctionType fn)
 	{
 		typedef typename std::result_of<FunctionType()>::type ResultType;
-		std::packaged_task<ResultType()> task{std::move(f)};
+		std::packaged_task<ResultType()> task{std::move(fn)};
 		std::future<ResultType> res{task.get_future()};
 		std::unique_lock lk{mMutex};
 		mWorkQueue.push(std::move(task));
@@ -95,17 +113,16 @@ public:
 		lk.unlock();
 
 		mWaitForTasks.notify_one();
-		for (auto &t: mThreads) {
-			if (t.joinable()) {
-				t.join();
+		for (auto &thread: mThreads) {
+			if (thread.joinable()) {
+				thread.join();
 			}
 		}
 	}
-private:
 
+private:
 	bool tryPop(FunctionWrapper &value)
 	{
-		//std::unique_lock<std::mutex> lk{mMutex};
 		if (mWorkQueue.empty()) {
 			return false;
 		}
@@ -116,7 +133,7 @@ private:
 
 	void workerThread()
 	{
-		unsigned yieldCounter;
+		unsigned yieldCounter{0};
 		while (true) {
 			FunctionWrapper task;
 			std::unique_lock lk{mMutex};
@@ -134,6 +151,7 @@ private:
 					if (mDone && (mWorkQueue.empty() || !mWaitForAllTasksToFinish)) {
 						break;
 					}
+					yieldCounter = 0;
 				}
 				else {
 					++yieldCounter;
@@ -144,11 +162,18 @@ private:
 		mWaitForTasks.notify_all();
 	}
 
+	/// True when the queue is finished and the worker threads should halt
 	bool mDone;
+	/// True if the worker threads should wait until all the tasks have been processed
 	bool mWaitForAllTasksToFinish;
+	/// Contains all the work to be done
 	std::queue<FunctionWrapper> mWorkQueue;
+	/// Spawned thread objects are stored here
 	std::vector<std::thread> mThreads;
+	/// A mutex
 	mutable std::mutex mMutex;
+	/// This condition is used to signal to the worker threads if an event has happened
 	std::condition_variable mWaitForTasks;
+	/// The thread loop will yield execution until this maximum was reached, then it "sleeps" by waiting on a CV
 	static constexpr auto cMaxYielding{500};
 };
